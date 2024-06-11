d7c6a0799a97c015f5d89f4373c233674e622895
Anastasiya-Zhoroven <anastasiya.zhoroven@gmail.com>
2024-06-11 15:49:05 +0200

    do task 6 from lesson 2

---
+++ docs/lesson-02/06.js
@@ -0,0 +1,13 @@
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+let passportWithAddress2 = JSON.parse(JSON.stringify(passportWithAddress));
+passportWithAddress2.address.country = "Belarus";
+passportWithAddress2.address.city = "Bobryisk";
+console.log(passportWithAddress.address.city);
+console.log(passportWithAddress2.address.city);
